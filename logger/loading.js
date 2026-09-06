1aa0cde86dd8bc37e114de039fff518b6d40aca9
siyam-Fms <dygdd022@gmail.com>
2026-09-07 02:35:47 +0600

    Update loading.js

--- logger/loading.js
+++ logger/loading.js
@@ -1,14 +1,14 @@
 const { colors } = require('../func/colors.js');
 const moment = require("moment-timezone");
 const characters = '';
-const getCurrentTime = () => colors.gray(moment().tz('Asia/Ho_Chi_Minh').format('HH:mm:ss DD/MM/YYYY'));
+const getCurrentTime = () => colors.gray(moment().tz('Asia/Dhaka').format('HH:mm:ss DD/MM/YYYY'));
 
 function logError(prefix, message) {
 	if (message === undefined) {
 		message = prefix;
 		prefix = "ERROR";
 	}
-	process.stderr.write(`\r${`${getCurrentTime()} ${colors.redBright(`${characters} ${prefix}:`)} ${message}`}`);
+	process.stderr.write(`\r${`${getCurrentTime()} ${colors.redBright(`${characters} ${prefix}:`)} ${message}\n`}`);
 }
 
 module.exports = {
@@ -19,27 +19,34 @@ module.exports = {
 			message = prefix;
 			prefix = "WARN";
 		}
-		process.stderr.write(`\r${`${getCurrentTime()} ${colors.yellowBright(`${characters} ${prefix}:`)} ${message}`}`);
+		process.stderr.write(`\r${`${getCurrentTime()} ${colors.yellowBright(`${characters} ${prefix}:`)} ${message}\n`}`);
 	},
 	info: function (prefix, message) {
 		if (message === undefined) {
 			message = prefix;
 			prefix = "INFO";
 		}
-		process.stderr.write(`\r${`${getCurrentTime()} ${colors.greenBright(`${characters} ${prefix}:`)} ${message}`}`);
+		process.stderr.write(`\r${`${getCurrentTime()} ${colors.greenBright(`${characters} ${prefix}:`)} ${message}\n`}`);
 	},
 	succes: function (prefix, message) {
 		if (message === undefined) {
 			message = prefix;
-			prefix = "SUCCES";
+			prefix = "SUCCESS";
 		}
-		process.stderr.write(`\r${`${getCurrentTime()} ${colors.cyanBright(`${characters} ${prefix}:`)} ${message}`}`);
+		process.stderr.write(`\r${`${getCurrentTime()} ${colors.cyanBright(`${characters} ${prefix}:`)} ${message}\n`}`);
+	},
+	success: function (prefix, message) {
+		if (message === undefined) {
+			message = prefix;
+			prefix = "SUCCESS";
+		}
+		process.stderr.write(`\r${`${getCurrentTime()} ${colors.cyanBright(`${characters} ${prefix}:`)} ${message}\n`}`);
 	},
 	master: function (prefix, message) {
 		if (message === undefined) {
 			message = prefix;
 			prefix = "MASTER";
 		}
-		process.stderr.write(`\r${`${getCurrentTime()} ${colors.hex("#eb6734", `${characters} ${prefix}:`)} ${message}`}`);
+		process.stderr.write(`\r${`${getCurrentTime()} ${colors.hex("#eb6734", `${characters} ${prefix}:`)} ${message}\n`}`);
 	}
 };
